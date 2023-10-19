67fd86e70abff3f14c4ae5653744e9ab5d147adb
HolmesPerfectC <343409317@qq.com>
2023-10-19 10:04:22 +0800

    remove Tutorials build files

--- Tutorials/build/my_composition/CMakeFiles/my_composition_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/my_composition_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/my_composition_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
